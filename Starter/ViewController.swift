05d46abe8fb2ca95e05912174c4ab3fe557e569f
htunminlatt <htunminlatt.iosdev@gmail.com>
2021-01-16 16:24:40 +0630

    Initial Commit

---
+++ Starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Starter
+//
+//  Created by Sai Xtun on 16/01/2021.
+//  Copyright © 2021 padc.com.mm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
